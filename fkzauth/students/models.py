2d57137bc15e6cd091e5a07aa137c49658db847d
Victor Berger <victor.berger@m4x.org>
2014-02-15 17:18:18 +0100

    Fixing create_user*.
    Signed-off-by: Victor Berger <victor.berger@m4x.org>

--- fkzauth/students/models.py
+++ fkzauth/students/models.py
@@ -9,7 +9,7 @@ class StudentManager(BaseUserManager):
     """
     Class managing students
     """
-    def create_user(self, firstname, lastname, email, promotions, forlife_schools, password):
+    def create_user(self, firstname, lastname, email, promotion, forlife, password):
         """
         Creates and saves the student identification informations
         """
@@ -17,28 +17,21 @@ class StudentManager(BaseUserManager):
             firstname=firstname,
             lastname=lastname,
             email=self.normalize_email(email),
-            promotions=promotions,
-            forlife_schools=forlife_schools
             )
-        
+        student.save(using=self._db)
+        student.promotions.add(promotion)
+        student.save(using=self._db)
+        sauth = SchoolAuth(student=student, school=promotion.formation.school, forlife=forlife)
         student.set_password(password)
         student.save(using=self._db)
         
         return student
         
-    def create_super_user(self, firstname, lastname, email, promotions, forlife_schools, password):
+    def create_superuser(self, firstname, lastname, email, promotion, forlife, password):
         """
         Creates and saves the infrormations about a superuser (= admin)
         """
-        student = self.model(
-            firstname=firstname,
-            lastname=lastname,
-            email=self.normalize_email(email),
-            promotions=promotions,
-            forlife_schools=forlife_schools
-            )
-            
-        student.set_password(password)
+        student = self.create_user(firstname, lastname, email, promotion, forlife, password)
         student.is_staff = True
         student.save(using=self._db)
         
