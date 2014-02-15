57c68907d6fd2c5a03c2368d294cc618018cba0b
AlexCid <schaub.alexander@free.fr>
2014-02-15 17:43:20 +0100

    correcting syntax errors on the authentication backend

--- fkzauth/students/backends.py
+++ fkzauth/students/backends.py
@@ -6,24 +6,22 @@ from fkzauth.students.models import Student
 
 class StudentBackend(object):
     def authenticate(self, forlife, school_id, password):
-    """ Authenticates a student using its email and school """
+        """ Authenticates a student using its email and school """
     
         try:
-            student = Student.StudentManager.select_related('promotion__formation__school')
-                                .get(forlife_schools__forlife=forlife)
-                                .get(promotions__formation__school__pk=school_id)
+            student = Student.StudentManager.select_related('promotion__formation__school').get(forlife_schools__forlife=forlife).get(promotions__formation__school__pk=school_id)
             # select_related is used for performance reasons
             
             if student.check_password(password):
                 return student
-            else
+            else:
                 return None              
         except Student.DoesNotExist:
             # Run the default password hasher once to prevent timing-attacks
             Student.set_password(password)   
             return None  
         
-    def get_user(self, user_id)
+    def get_user(self, user_id):
         
         try:
             return Student.StudentManager.get(pk=user_id)
