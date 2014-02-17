48052ff157e32ac0017c47cffe9e5f231909d08f
Victor Berger <victor.berger@m4x.org>
2014-02-17 09:32:29 +0100

    Templates cleaning.
    Signed-off-by: Victor Berger <victor.berger@m4x.org>

--- fkzauth/settings.py
+++ fkzauth/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+FKZA_DIR = os.path.join(BASE_DIR, 'fkzauth')
 
 
 # Quick-start development settings - unsuitable for production
@@ -104,8 +105,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, 'templates'),
-    
+    os.path.join(FKZA_DIR, 'templates')
 )
 
 
